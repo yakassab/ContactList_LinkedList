65050221d2f367fc8154eacc08f70262c8501778
Youssef Kassab <21p0010@eng.asu.edu.eg>
2023-10-31 19:30:59 +0200

    kol el functions gahza

--- ContactList_LinkedList/Contact.cpp
+++ ContactList_LinkedList/Contact.cpp
@@ -1,1 +1,109 @@
 #include "Contact.h"
+#include <regex>
+Contact::Contact()
+{
+}
+
+Contact::Contact(string name, string phone, string email, string address)
+{
+	this->name = name;
+	this->phoneNumber = phone;
+	this->email = email;
+	this->address = address;
+}
+
+Contact::~Contact()
+{
+}
+
+string Contact::getName()
+{
+	return name;
+}
+
+string Contact::getPhone()
+{
+	return phoneNumber;
+}
+
+string Contact::getEmail()
+{
+	return email;
+}
+
+string Contact::getAddress()
+{
+	return address;
+}	
+
+void Contact::setName(string name)
+{
+	this->name = name;
+}	
+
+void Contact::setPhoneNumber(string phone)
+{
+	this->phoneNumber = phone;
+}	
+
+void Contact::setEmail(string email)
+{
+	this->email = email;
+}
+
+void Contact::setAddress(string address)
+{
+	this->address = address;
+}
+
+bool Contact::isValidName(string name) {
+	// Check if the name is empty.
+	if (name.empty()) {
+		return false;
+	}
+	// Check if the first character is a letter.
+	if (!isalpha(name[0])) {
+		return false;
+	}
+	return true;
+}
+
+bool Contact::isValidPhone(string phoneNumber)
+{
+	// Check if the phone number is 11 characters long.
+	if (phoneNumber.size() != 11) {
+		return false;
+	}
+
+	// Check if the phone number starts with 01.
+	if (phoneNumber.substr(0, 2) != "01") {
+		return false;
+	}
+
+	// Check if the remaining characters are digits.
+	for (int i = 2; i < phoneNumber.size(); ++i) {
+		if (!isdigit(phoneNumber[i])) {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool Contact::isValidEmail(string email)
+{
+	regex emailRegex("[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*");
+
+	// Match the email address against the regular expression.
+	return regex_match(email, emailRegex);
+}
+
+bool Contact::isValidAddress(string address)
+{
+	return !address.empty();
+}
+
+
+
+
+
+
--- ContactList_LinkedList/Contact.h
+++ ContactList_LinkedList/Contact.h
@@ -5,11 +5,28 @@ class Contact
 {
 private:
 	string name;
-	string phone;
+	string phoneNumber;
 	string email;
 	string address;
 
 public:
+	Contact();
+	Contact(string name, string phoneNumber, string email, string address);
+	~Contact();
 
+	//---------Getters & Setters------------//
+	string getName();
+	string getPhone();
+	string getEmail();
+	string getAddress();
+	void setName(string name);
+	void setPhoneNumber(string phoneNumber);
+	void setEmail(string email);
+	void setAddress(string address);
+	//----------------Checks----------------//
+	bool isValidName(string name);
+	bool isValidPhone(string phoneNumber);
+	bool isValidEmail(string email);
+	bool isValidAddress(string address);
 };
 
